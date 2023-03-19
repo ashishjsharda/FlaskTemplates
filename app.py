f75520e230745a5c347454d24df35f989feabc2d
Ashish Sharda <ashishjsharda@gmail.com>
2023-03-19 08:11:34 -0500

    update app

--- app.py
+++ app.py
@@ -5,7 +5,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    user = {'username': 'Ashish'}
+    return '''
+    <html>
+        <head>
+            <title>Home Page - Microblog</title>
+        </head>
+        <body>
+            <h1>Hello, ''' + user['username'] + '''!</h1>
+        </body>
+    </html>'''
+
 
 
 if __name__ == '__main__':
